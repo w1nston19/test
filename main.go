664edd0705fb2310faa5cb9cd298f9421730b2eb
Mihail Zonev <mihail.zonev@sap.com>
2024-11-25 22:39:32 +0200

    init

---
+++ main.go
@@ -0,0 +1,12 @@
+package test
+
+import (
+	"os"
+	s "test/test/sleeper"
+	"time"
+)
+
+func main() {
+	sleeper := &s.ConfigurableSleeper{1 * time.Second, time.Sleep}
+	s.Countdown(os.Stdout, sleeper)
+}
